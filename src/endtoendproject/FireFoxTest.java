a6843d41f2fcb76f2a2e697f2f3833d207933ca2
Nisum <nisum@Admins-MacBook-Pro.local>
2017-06-28 15:20:26 -0700

    This change is for adding hi statement

--- src/endtoendproject/FireFoxTest.java
+++ src/endtoendproject/FireFoxTest.java
@@ -12,6 +12,7 @@ public class FireFoxTest {
 		System.out.println("Hello World");
 		System.setProperty("webdriver.gecko.driver","/Users/Arun/Practical Programs/EndtoEndProject/geckodriver");
 		driver.get("/http://chromedriver.storage.googleapis.com/index.html?path=2.30/");
+		System.out.println("Hi");
 		Thread.sleep(3000);
 			driver.close();
 
