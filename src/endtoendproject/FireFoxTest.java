a91c57711ddda33192c79e944544b3c8405e6d70
Nisum <nisum@Admins-MacBook-Pro.local>
2017-06-28 15:15:53 -0700

    MESSAGE

---
+++ src/endtoendproject/FireFoxTest.java
@@ -0,0 +1,20 @@
+package endtoendproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxTest {
+	
+	
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		System.out.println("Hello World");
+		System.setProperty("webdriver.gecko.driver","/Users/Arun/Practical Programs/EndtoEndProject/geckodriver");
+		driver.get("/http://chromedriver.storage.googleapis.com/index.html?path=2.30/");
+		Thread.sleep(3000);
+			driver.close();
+
+	}
+
+}
